547fc02a193b594aa94a3a686ddfaf6bec1dea0c
Austin <p28aschreffler@fairfieldprep.org>
2026-01-23 09:09:30 -0500

    Submit cycle 10 day 1 labs

---
+++ lab_10-3.py
@@ -0,0 +1,16 @@
+# author: Austin Schreffler 01/21/2026
+print(123)
+print(20+20)
+print(2 * 4 * 6 * 8)
+print(0/13)
+print(2 ** 3)
+print(+321)
+print(2020-4)
+print(9/5)
+print(3 % 10)
+2 * 2 * 2
+-321
+20 + 20
+9//5
+3 % 10
+#    0/0
